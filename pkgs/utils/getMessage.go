f17f6636c2af93d09172e91d8bedd2ba03525f32
mendoza000 <omarmendoza.alv25@gmail.com>
2024-08-02 15:49:43 -0400

    eliminando espacios

--- pkgs/utils/getMessage.go
+++ pkgs/utils/getMessage.go
@@ -2,23 +2,27 @@ package utils
 
 import (
 	"github.com/golang/protobuf/proto"
+	"strings"
 	"wabot/models"
 )
 
 func GetMessage(reminder bool, client models.Client, service string, account models.Account) *string {
 
-	msg := proto.String("*Mensaje automatizado* \n\nHola, buen dia 🙌. \nQueriamos recordate que *pronto vencerá la cuenta de " + service + "*, asociada al correo: " + account.Mail + ". \n\nPor favor, mantente al tanto de este aviso y asegurate de tener todo listo para el pago correspondiente. \n\n¡Gracias por tu continua confianza y preferencia! 🙌")
+	trimService := strings.TrimSpace(service)
+	trimName := strings.TrimSpace(client.Name)
+
+	msg := proto.String("*Mensaje automatizado* \n\nHola, buen dia 🙌. \nQueriamos recordate que *pronto vencerá la cuenta de " + trimService + "*, asociada al correo: " + account.Mail + ". \n\nPor favor, mantente al tanto de este aviso y asegurate de tener todo listo para el pago correspondiente. \n\n¡Gracias por tu continua confianza y preferencia! 🙌")
 
 	if client.IsResellerCustomer {
-		msg = proto.String("*Mensaje automatizado* \n\nHola, buen dia 🙌. \nQueremos recordate que la cuenta de *" + service + "* a nombre de *" + client.Name + "* está por vecer pronto.\n\nAgredecemos tu atención y getión al respecto.")
+		msg = proto.String("*Mensaje automatizado* \n\nHola, buen dia 🙌. \nQueremos recordate que la cuenta de *" + trimService + "* a nombre de *" + trimName + "* está por vencer pronto.\n\nAgredecemos tu atención y getión al respecto.")
 	}
 
 	if reminder {
-		msg = proto.String("*Mensaje automatizado* \n\nHola, buen dia 🙌. \nSolo un recordatorio sobre su pago pendiente de *" + service + "*, asociado al correo: *" + account.Mail + "*. \n\nAgradecemos su pronta gestión. 🙌")
+		msg = proto.String("*Mensaje automatizado* \n\nHola, buen dia 🙌. \nSolo un recordatorio sobre su pago pendiente de *" + trimService + "*, asociado al correo: *" + account.Mail + "*. \n\nAgradecemos su pronta gestión. 🙌")
 	}
 
 	if reminder && client.IsResellerCustomer {
-		msg = proto.String("*Mensaje automatizado* \n\nHola, buen dia 🙌. \nSolo un recordatorio sobre el pago pendiente de *" + service + "* a nombre de *" + client.Name + "*, asociado al correo: *" + account.Mail + "*. \n\nAgradecemos su pronta gestión. 🙌")
+		msg = proto.String("*Mensaje automatizado* \n\nHola, buen dia 🙌. \nSolo un recordatorio sobre el pago pendiente de *" + trimService + "* a nombre de *" + trimName + "*, asociado al correo: *" + account.Mail + "*. \n\nAgradecemos su pronta gestión. 🙌")
 	}
 
 	return msg
